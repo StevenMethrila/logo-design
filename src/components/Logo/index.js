e0e067418dbaa7783907589351221364f57d0407
steven <rajsteven524@gmail.com>
2024-08-30 16:21:33 +0530

    solution

---
+++ src/components/Logo/index.js
@@ -0,0 +1,16 @@
+import './index.css';  
+
+const Logo = () => {
+    return (
+        <div className="logo-container">
+            <div className="diamond-container">
+                <div className="diamond-information">
+                    <h1 className="main-heading">HTML <span className="spanElement">&</span> CSS</h1>
+                    <p className="main-para">design and build websites</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Logo;
